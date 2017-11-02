1b07eab74c3f983802fa4e0631906595fac8451f
Hung Nguyen <nmhung1210@gmail.com>
2017-11-02 20:50:00 +0700

    + Adding first stable version

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+agent_conf_file="/etc/amplify-agent/agent.conf"
+sed -i.old -e "s/api_key.*$/api_key = ${API_KEY}/" ${agent_conf_file}
+sed -i.old -e "s/imagename.*$/imagename = ${AMPLIFY_IMAGENAME}/" ${agent_conf_file}
+
+nginx -g "daemon off;" &
+nginx_pid=$!
+
+python /usr/bin/nginx-amplify-agent.py start --config=/etc/amplify-agent/agent.conf --pid=/var/run/amplify-agent/amplify-agent.pid
+
+wait ${nginx_pid}
